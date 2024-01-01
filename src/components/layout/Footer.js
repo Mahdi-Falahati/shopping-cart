42cef67b727fc2d8704365597275e1226b5eb365
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-01 03:43:27 -0800

    complete footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import { Typography } from "@mui/material";
+import FollowTheSignsOutlinedIcon from "@mui/icons-material/FollowTheSignsOutlined";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Typography
+      variant="button"
+      textAlign="center"
+      display="block"
+      pt={2}
+      sx={{borderTop:"1px dotted #000"}}
+    >
+      Created By
+      <a href="https://github.com/Mahdi-Falahati" target="_blank">
+        <FollowTheSignsOutlinedIcon sx={{ margin: "0px 5px" }} />
+        Mahdi-Falahati
+      </a>
+    </Typography>
+  );
 }
