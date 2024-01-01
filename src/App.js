b3ed317d4dc164afaffab59af313e68b2e9066bb
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-01 02:22:50 -0800

    make base root and root layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import RootLayout from "./components/layout/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+    </Route>
+  )
+);
+
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
