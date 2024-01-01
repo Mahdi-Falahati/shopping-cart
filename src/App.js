b61a22b4fe768807a36b7b4d67b5eadeb0f5f291
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-01 11:57:19 -0800

    get data from api and storage to redux store

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ import {
 } from "react-router-dom";
 
 import RootLayout from "./components/layout/RootLayout";
+import Home from "./pages/Home";
+import { useEffect } from "react";
+import Api from "./utils/Api";
+import { useDispatch } from "react-redux";
+import { fetchingData } from "./features/ProductSlice";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
     </Route>
   )
 );
 
-
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const data = async () => {
+      const response = await Api(
+        "https://api-react-shopping-cart.vercel.app/data"
+      );
+      dispatch(fetchingData(response.data));
+    };
+    data();
+  }, [dispatch]);
+
   return <RouterProvider router={router} />;
 }
 
