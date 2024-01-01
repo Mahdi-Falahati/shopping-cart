70d3f86ecfcb3b55c42b27100c0006e0da055f95
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-01 02:57:09 -0800

    complete header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Badge, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import styles from './layout.module.css';
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={styles.header}>
+      <Stack mx={4} alignItems="center" justifyContent="space-between" flexDirection="row">
+        <Typography variant="h4" fontWeight="bold" color="primary">
+          Store
+        </Typography>
+        <Link to="/cart">
+          <Badge badgeContent={4} color="warning">
+            <ShoppingCartOutlinedIcon fontSize="large" />
+          </Badge>
+        </Link>
+      </Stack>
+    </header>
+  );
 }
