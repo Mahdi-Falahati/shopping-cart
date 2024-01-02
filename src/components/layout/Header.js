d4e6891d3db97febb05303639c24ab4a193748d2
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-02 04:13:56 -0800

    add link to title in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,7 +8,9 @@ export default function Header() {
     <header className={styles.header}>
       <Stack mx={4} alignItems="center" justifyContent="space-between" flexDirection="row">
         <Typography variant="h5" fontWeight="bold" color="primary">
+          <Link to={`/`} style={{ textDecoration:"none",color:"blue" }}>
           Store
+        </Link>
         </Typography>
         <Link to="/cart">
           <Badge badgeContent={4} color="warning">
