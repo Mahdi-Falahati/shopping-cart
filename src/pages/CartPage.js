7e8d13007464377646fe5b25b0110203418046cc
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-03 10:13:52 -0800

    complete cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,35 +1,59 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import CleaningServicesOutlinedIcon from "@mui/icons-material/CleaningServicesOutlined";
 import ShoppingCartCheckoutOutlinedIcon from "@mui/icons-material/ShoppingCartCheckoutOutlined";
 import shopingcart from "../assets/shopingcart.svg";
 import { Link } from "react-router-dom";
+import ProductCard from '../components/modules/ProductCard';
+import { clearDataSelected } from "../features/ProductSlice";
 
 export default function CartPage() {
+  const selected = useSelector((state) => state.products.selectedProduct);
+  const dispatch = useDispatch()
+
+  let totalPrice = 0;
+  for (const item of selected) {
+    totalPrice += item.price;
+  }
+
+  const clearProductHandler = ()=>{
+    dispatch(clearDataSelected())
+  }
+
   return (
     <Box sx={{ flexGrow: 1, minHeight: "81vh" }}>
-      <Grid container spacing={2} justifyContent="center" alignItems="center">
-        <Grid item xs={11} mt={4} sm={7}>
-          <Stack justifyContent="center" alignItems="center">
-            <img
-              src={shopingcart}
-              style={{ height: "200px" }}
-              alt="shoping cart"
-            />
-            <Typography variant="h6">Your Cart is Currently Empty!</Typography>
-            <Link to="/" style={{ marginTop: "20px" }}>
-              <Button variant="contained" size="large" sx={{ width: "260px" }}>
-                Back to Store
-              </Button>
-            </Link>
-          </Stack>
+      <Grid container spacing={2} justifyContent="center" alignItems="flex-start">
+        <Grid item xs={11} mt={4} sm={7} display="flex" justifyContent="space-around" flexDirection="row" flexWrap="wrap"  sx={{height:"75vh",overflow: "auto"}}>
+          {selected.length > 0 ? (
+            selected.map((item) => <ProductCard key={item.id} {...item} />)
+          ) : (
+            <Stack justifyContent="center" alignItems="center">
+              <img
+                src={shopingcart}
+                style={{ height: "200px" }}
+                alt="shoping cart"
+              />
+              <Typography variant="h6">
+                Your Cart is Currently Empty!
+              </Typography>
+              <Link to="/" style={{ marginTop: "20px" }}>
+                <Button
+                  variant="contained"
+                  size="large"
+                  sx={{ width: "260px" }}
+                >
+                  Back to Store
+                </Button>
+              </Link>
+            </Stack>
+          )}
         </Grid>
         <Grid item xs={11} mt={4} sm={4} md={3}>
           <Typography color="primary" variant="h6">
-            Total Products: 0
+            Total Products: {selected.length}
           </Typography>
           <Typography color="primary" variant="h6">
-            Total Price: 0 $
+            Total Price: {totalPrice.toFixed(3)} $
           </Typography>
           <Stack
             mt={2}
@@ -41,6 +65,7 @@ export default function CartPage() {
               sx={{ margin: "10px 0px" }}
               variant="contained"
               color="warning"
+              onClick={clearProductHandler}
               startIcon={<CleaningServicesOutlinedIcon />}
             >
               Clear All
@@ -48,6 +73,7 @@ export default function CartPage() {
             <Button
               sx={{ margin: "10px 0px" }}
               color="success"
+              onClick={clearProductHandler}
               variant="contained"
               endIcon={<ShoppingCartCheckoutOutlinedIcon />}
             >
