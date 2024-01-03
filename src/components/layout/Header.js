cafd9cf1a6fa48e136d1c8ba5138ba2182e75677
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2024-01-02 16:11:38 -0800

    add count selec product as badge

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,27 @@
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { Badge, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import styles from '../templates/layout.module.css';
+import styles from "../templates/layout.module.css";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const count = useSelector((state) => state.selectProducts.counter);
+
   return (
     <header className={styles.header}>
-      <Stack mx={4} alignItems="center" justifyContent="space-between" flexDirection="row">
+      <Stack
+        mx={4}
+        alignItems="center"
+        justifyContent="space-between"
+        flexDirection="row"
+      >
         <Typography variant="h5" fontWeight="bold" color="primary">
-          <Link to={`/`} style={{ textDecoration:"none",color:"blue" }}>
-          Store
-        </Link>
+          <Link to={`/`} style={{ textDecoration: "none", color: "blue" }}>
+            Store
+          </Link>
         </Typography>
         <Link to="/cart">
-          <Badge badgeContent={4} color="warning">
+          <Badge badgeContent={count} color="warning">
             <ShoppingCartOutlinedIcon fontSize="large" />
           </Badge>
         </Link>
